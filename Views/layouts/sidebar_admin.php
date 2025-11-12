07a6565e0ef738d2c9a7ba5c8b9753867618580b
wulan <wulandewipramesti@gmail.com>
2025-11-12 08:45:13 +0800

    Update sidebar_admin.php

--- Views/layouts/sidebar_admin.php
+++ Views/layouts/sidebar_admin.php
@@ -174,16 +174,7 @@
             </div>
             <!-- End of Main Content -->
 
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
-                    </div>
-                </div>
-            </footer>
-
-        </div>
+        
         <!-- End of Content Wrapper -->
 
     </div>
